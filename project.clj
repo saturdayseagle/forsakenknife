da733efa936abea9dc7969851584b75e002426d3
Emory Merryman <emory.merryman@gmail.com>
2015-11-11 03:12:04 -0500

    using file repo

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories [["net", "file:///home/vagrant/working/easterntombstone/forsakenknife"]]
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot forsakenknife.core
   :target-path "target/%s"
