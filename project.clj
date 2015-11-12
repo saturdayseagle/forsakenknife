0782534a0c13d3e99b7e1a04a8a85dc0506c7e65
Emory Merryman <emory.merryman@gmail.com>
2015-11-12 00:22:50 -0500

    1/2

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
-(defproject forsakenknife "0.1.0-SNAPSHOT"
+(defproject saturdayseagle/forsakenknife "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories [["net", "https://raw.githubusercontent.com/easterntombstone/loneviper/master"]]
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :repositories [
+  		["file", "file:///home/vagrant/working/easterntombstone/forsakenknife/loneviper"]
+;  		["net", {:url "https://raw.githubusercontent.com/easterntombstone/loneviper/master"}]
+  ]
+  :dependencies [[org.clojure/clojure "1.7.0"] [easterntombstone/forsakenknife "0.2.0-SNAPSHOT"]]
   :main ^:skip-aot forsakenknife.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/forsakenknife/core_test.clj
+++ test/forsakenknife/core_test.clj
@@ -1,7 +1,13 @@
 (ns forsakenknife.core-test
-  (:require [clojure.test :refer :all]
-            [forsakenknife.core :refer :all]))
+  (:require [clojure.test :refer :all]))
+;	    (:require [easterntombstone/forsakenknife.core])
 
 (deftest a-test
   (testing "FIXME, I fail."
     (is (= 0 1))))
+
+(deftest b-test
+  (testing "FIXME, I fail."
+;    (println (foo "NO"))
+    (is (= 0 1))
+    ))
